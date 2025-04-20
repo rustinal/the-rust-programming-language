444d7043f0bdaf8e59877b74d11ca6c49acc1db5
auro.mohanty <aurogreenindia@gmail.com>
2025-04-20 18:36:09 +0200

    feat: add parameters to another_function

--- common_programming_concepts/functions/src/main.rs
+++ common_programming_concepts/functions/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    println!("Hello, world!");
-    another_function();
+    another_function(5, 'h');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
